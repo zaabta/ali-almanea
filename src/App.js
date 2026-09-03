a7984d208884231b5dfd453176f1561f25bf88ab
zabtaa <ali.1995almanea@gmail.com>
2026-09-04 01:32:40 +0300

    Polish article navigation and assets

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useLayoutEffect, useRef } from 'react';
+import { useLocation, useNavigationType, Route, Routes } from 'react-router-dom';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Hero from './pages/Hero';
 import About from './pages/About';
 import Layout from './components/layout';
@@ -6,16 +9,42 @@ import Seo from './components/seo';
 import Blog, { ArticlePage } from './pages/Blog';
 import articles from './data/articles';
 import NotFound from './pages/NotFound';
-import { Route, Routes } from 'react-router-dom';
+
+const RouteScrollManager = () => {
+  const { pathname, hash } = useLocation();
+  const navigationType = useNavigationType();
+  const firstRender = useRef(true);
+
+  useLayoutEffect(() => {
+    const shouldReset = !hash && (firstRender.current || navigationType !== 'POP');
+    const resetScroll = () => {
+      if (shouldReset) window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    };
+
+    resetScroll();
+    firstRender.current = false;
+
+    const frame = window.requestAnimationFrame(() => {
+      ScrollTrigger.refresh();
+      resetScroll();
+    });
+    return () => window.cancelAnimationFrame(frame);
+  }, [pathname, hash, navigationType]);
+
+  return null;
+};
 
 const App = () => {
   return (
-    <Routes>
+    <>
+      <RouteScrollManager />
+      <Routes>
       <Route path="/" element={<div className="App"><Seo /><Layout><Hero /><About/><Portfolio /></Layout></div>} />
       <Route path="/blog" element={<div className="App"><Seo blog /><Blog /></div>} />
       {articles.map((article) => <Route key={article.slug} path={`/blog/${article.slug}`} element={<div className="App"><Seo article={article} /><ArticlePage article={article} /></div>} />)}
       <Route path="*" element={<div className="App"><NotFound /></div>} />
-    </Routes>
+      </Routes>
+    </>
   );
 }
 
